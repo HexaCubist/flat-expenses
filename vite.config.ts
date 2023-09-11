edf80ab2a6c2ec661f4f029559f1cd4183e9cbcc
Zac M-W <z.millerwaugh@auckland.ac.nz>
2023-09-12 09:50:16 +1200

    Use CJS Interop plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { cjsInterop } from 'vite-plugin-cjs-interop';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		cjsInterop({
+			dependencies: ['@haverstack/axios-fetch-adapter']
+		}),
+		sveltekit()
+	]
 });
