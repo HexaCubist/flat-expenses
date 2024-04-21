858a586eb38076a5a1d5dec6ab923d3a3dff93b6
Zac M-W <z.millerwaugh@auckland.ac.nz>
2024-04-21 16:55:22 +1200

    Fix typo in config

--- chatbot/index.js
+++ chatbot/index.js
@@ -8,12 +8,12 @@ if (!process.env.FB_APPSTATE) {
 const bree = new Bree({
 	jobs: [
 		// 'chore-roster',
-		'bins-reminder'
+		'bins-reminder',
 		{
 			name: 'chore-roster',
 			// At 7:14am every Monday
 			cron: process.env.CRON_CHORE_ROSTER
-		},
+		}
 		// {
 		// 	name: 'bins-reminder',
 		// 	// Every evening at 4:30pm
